80e1d0d7a13bfba1d8f32454a7c3d2f36362e8c1
sammyne <xiangminli@alumni.sjtu.edu.cn>
2024-07-12 01:09:15 +0000

    feat(guest-c): demo printf

--- guest-c/src/greeter.c
+++ guest-c/src/greeter.c
@@ -9,10 +9,13 @@ void greeter_greet(greeter_string_t *who, greeter_string_t *ret) {
   const size_t N = strlen(greeting);
   const size_t M = N + who->len;
 
-  uint8_t *out = malloc(M);
+  uint8_t *out = calloc(M+1, sizeof(uint8_t));
 
   memcpy(out, greeting, N);
   memcpy(out + N, who->ptr, who->len);
+
+  printf("%s\n", out);
+  printf("len(out)=%zu\n", M);
 
   ret->ptr = out;
   ret->len = M;
@@ -20,4 +23,5 @@ void greeter_greet(greeter_string_t *who, greeter_string_t *ret) {
 
 void greeter_say_hi(greeter_string_t *ret) {
   greeter_string_set(ret, "hi :)");
+  printf("hello world\n");
 }
